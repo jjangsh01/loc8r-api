35d7197279e812c2ec265c02e0c874b2ea4c5ae1
jjangsh01 <150315609+jjangsh01@users.noreply.github.com>
2024-10-28 03:52:18 +0900

    Add files via upload

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,18 +1,12 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const ctrlLocations = require("../controllers/locations");
-const ctrlOthers = require("../controllers/others");
+const ctrlLocations = require('../controllers/locations');
+const ctrlOthers = require('../controllers/others');
 
-router.get("/", ctrlLocations.homelist);
-router.get("/location", ctrlLocations.locationInfo);
-router.get("/location/:locationid", ctrlLocations.locationInfo);
-router.get("/location/review/new", ctrlLocations.addReview);
+router.get('/', ctrlLocations.homelist);
+router.get('/location/:locationid', ctrlLocations.locationInfo);
+router.get('/location/review/new', ctrlLocations.addReview);
 
-// locations pages
-router.get("/", ctrlLocations.homelist);
-router.get("/location/:locationid", ctrlLocations.locationInfo);
-router.route("/location/:locationid/review/new").get(ctrlLocations.addReview).post(ctrlLocations.doAddReview);
-
-router.get("/about", ctrlOthers.about);
+router.get('/about', ctrlOthers.about);
 
 module.exports = router;
